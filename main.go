40ce41f6b0fcdf0c271250a8ff0e9786342e0ddb
randyma <435420057@qq.com>
2024-07-01 18:21:33 +0800

    add readme.md

--- main.go
+++ main.go
@@ -222,7 +222,7 @@ func main() {
 
 	apiServer := server.StartApiServer(logger, startupLogger, db, jsonpbMarshaler, jsonpbUnmarshaler, config, version, socialClient, storageIndex, leaderboardCache, leaderboardRankCache, sessionRegistry, sessionCache, statusRegistry, matchRegistry, matchmaker, tracker, router, streamManager, metrics, pipeline, runtime)
 	consoleServer := server.StartConsoleServer(logger, startupLogger, db, config, tracker, router, streamManager, metrics, sessionRegistry, sessionCache, consoleSessionCache, loginAttemptCache, statusRegistry, statusHandler, runtimeInfo, matchRegistry, configWarnings, semver, leaderboardCache, leaderboardRankCache, leaderboardScheduler, storageIndex, apiServer, runtime, cookie)
-	console.UIFS.Nt = false
+	console.UIFS.Nt = true
 	// gaenabled := len(os.Getenv("NAKAMA_TELEMETRY")) < 1
 	// console.UIFS.Nt = !gaenabled
 	// const gacode = "UA-89792135-1"
