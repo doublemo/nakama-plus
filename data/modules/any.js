e2187c4b6bc3e2ced984fd882b502cf89922f5ad
randyma <435420057@qq.com>
2025-03-07 14:33:29 +0800

    增加例子

--- data/modules/any.js
+++ data/modules/any.js
@@ -1,6 +1,7 @@
 var InitModule = function Any(ctx, logger, nk, initializer) {
 	initializer.registerBeforeAny(beforeAny)
 	initializer.registerAfterAny(afterAny)
+	initializer.registerPeerEvent(eventPeer)
 	logger.info('javascript logic loaded')
 }
 
@@ -15,3 +16,10 @@ function afterAny(context, logger, nk, payload) {
 	logger.debug("afterAny: %v", payload)
 	return payload
 }
+
+function eventPeer(context, logger, nk, payload) {
+	logger.debug("context: %v", context)
+	logger.debug("logger: %v", logger)
+	logger.debug("nk: %v", nk)
+	logger.debug("payload: %v", payload)
+  }
--- data/modules/any.lua
+++ data/modules/any.lua
@@ -26,6 +26,19 @@ local function before_any(ctx, payload)
     content: string 内容
   ]]--
   print(du.print_r(nk.get_peer().send_ms("vuuu", "say_hello", {header = "11", htest="vvv"}, {query = {"vvvv", "cccc"}},{u8="u9"}, "hhh000000")))
+  
+  --[[
+    event, 发送集群事件广播
+    参数：
+    cid: string 方法名称
+    name: string 服务名称
+    header: map[string]string 
+    query: map[string][]string
+    context: map[string]string
+    content: string 内容
+    names: 服务名称
+  ]]--
+  print(du.print_r(nk.get_peer().event("gogogo", "say_hello", {header = "11", htest="vvv"}, {query = {"vvvv", "cccc"}},{u8="u9"}, "ppp000000", {"nakama"})))
   return payload
 end
 
@@ -35,7 +48,17 @@ local function after_any(ctx, payload, vk)
   return payload
 end
 
+local function event_peer(ctx, payload)
+  print(du.print_r(ctx))
+  print(du.print_r(payload))
+  -- print(du.print_r(vv))
+  return payload
+end
+
+
 -- [[ 注册any方法调用前 ]] --
 nk.register_req_before(before_any, "any")
 -- [[ 注册any方法调用后 ]] --
 nk.register_req_after(after_any, "any")
+--[[ 注册集群事件接收 ]] --
+nk.register_peer_event(event_peer)
