59547ccee15ef425451dcfd50fb376de927d1476
randyma <435420057@qq.com>
2025-08-18 09:39:54 +0800

    Clear method only needs to delete once by prefix

--- internal/gocache/store/etcd/etcd.go
+++ internal/gocache/store/etcd/etcd.go
@@ -258,15 +258,9 @@ func (s *EtcdStore) tagKeys(key ...string) string {
 
 // Clear resets all data in the store
 func (s *EtcdStore) Clear(ctx context.Context) error {
-	// 使用keys方法构建正确的键前缀
-	prefix := s.keys()
+	newKeys := []string{strings.Trim(s.prefix, "/"), EtcdPrefix}
+	prefix := "/" + strings.Join(newKeys, "/")
 	_, err := s.client.Delete(ctx, prefix, clientv3.WithPrefix())
-	if err != nil {
-		return err
-	}
-	// 还需要清理所有的 tags
-	tagsPrefix := s.tagKeys()
-	_, err = s.client.Delete(ctx, tagsPrefix, clientv3.WithPrefix())
 	return err
 }
 
